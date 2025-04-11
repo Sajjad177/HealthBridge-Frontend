a859dde3078a49f7d73bf06feaf0e03f97c541e9
sajjad <sajjadsajjad098765@gmail.com>
2025-04-11 20:10:30 +0600

    authSlice in redux created

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,37 @@
+//* stap: 3
+
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+import { User } from "../../../types/globalTypes";
+
+const initialState = {
+  user: null,
+  token: null,
+  selectedUser: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setSelectedUser: (state, action) => {
+      state.selectedUser = action.payload;
+    },
+  },
+});
+
+export const { setUser, logOut, setSelectedUser } = authSlice.actions;
+export default authSlice.reducer;
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const selectCurrentUser = (state: RootState): User | null =>
+  state?.auth?.user;
